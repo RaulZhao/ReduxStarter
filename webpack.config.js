bdd3459df9c7b31f7b528456da014a28ce9ab821
Mason Shin <mason.shin@spirent.com>
2016-09-15 22:15:15 -0700

    Set webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css?sourceMap", "sass?sourceMap"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', 'jsx']
+  },
+  devServer: {
+    historyApiFailback: true,
+    contentBase: './',
+    port: 4000
+  }
+};
