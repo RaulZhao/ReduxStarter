e8f9ea72506b67cc141be8af64af256ed88fe6c0
Mason Shin <mason.shin@spirent.com>
2016-09-15 22:16:34 -0700

    Add example app with material-ui

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { white, amber500 } from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+const muiTheme = getMuiTheme({
+  appBar: {
+    color: amber500,
+    height: 50
+  }
+})
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <AppBar title="Hello World!" />
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
